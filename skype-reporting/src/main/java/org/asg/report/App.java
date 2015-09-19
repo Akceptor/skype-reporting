d70502992703467b14d3a93acff699ecd8ba9f30
vostapiv <akceptor@gmail.com>
2015-09-19 09:41:03 +0300

    Update

--- skype-reporting/src/main/java/org/asg/report/App.java
+++ skype-reporting/src/main/java/org/asg/report/App.java
@@ -1,7 +1,8 @@
 package org.asg.report;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import com.skype.ChatMessage;
 import com.skype.ChatMessageAdapter;
@@ -15,9 +16,17 @@ import com.skype.SkypeException;
 public class App {
 
 	@SuppressWarnings("nls")
-	static List<String> allowedUsers = Arrays.asList(new String[] { "akceptor.motofan.ru", "yankolyaspas" });
+	static Map<String, String> NICKNAMES = new HashMap<String, String>() {
+		{
+			put("akceptor.motofan.ru", "VO");
+			put("yankolyaspas", "MY");
+		}
+	};
+	static Set<String> allowedUsers = NICKNAMES.keySet();
+	static Map<String, String> statuses = new HashMap<String, String>();
 
 	public static void main(String[] args) throws SkypeException {
+		System.out.println("Please, bring Skype to foreground!"); //$NON-NLS-1$
 		Skype.setDaemon(false); // to prevent exiting from this program
 
 		Skype.addChatMessageListener(new ChatMessageAdapter() {
@@ -32,13 +41,28 @@ public class App {
 				}
 			}
 
-			private void processStatus(ChatMessage received) throws SkypeException {
-				@SuppressWarnings("nls")
-				String log = "[" + received.getTime() + "] " + received.getSenderId() + " says: "
-						+ received.getContent();
-				System.out.println(log);
-				received.getSender().send(log);
-			}
 		});
+
+	}
+
+	static void processStatus(ChatMessage received) throws SkypeException {
+		statuses.put(received.getSenderId(), received.getContent());
+		if (statuses.size() == allowedUsers.size()) {// last user
+			System.out.println(statuses);
+			received.getSender().send(buildStatus(statuses));
+			statuses.clear();
+		} else {
+			System.out.println("Received " + statuses.size() + " statuses for " + allowedUsers.size() + " users"); //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+			received.getSender().send("Status saved: " + received.getContent()); //$NON-NLS-1$
+		}
+		System.out.println(statuses);
+	}
+
+	private static String buildStatus(Map<String, String> userStatuses) {
+		StringBuilder statusBuilder = new StringBuilder("Status:\n"); //$NON-NLS-1$
+		for (String userId : userStatuses.keySet()) {
+			statusBuilder.append(NICKNAMES.get(userId)).append(": ").append(userStatuses.get(userId)).append("\n"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		return statusBuilder.toString();
 	}
 }

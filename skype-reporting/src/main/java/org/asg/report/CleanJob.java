1530d35baba1c35be251cfa62082bc3da02bdab5
Kolya Yanchiy <yankolyaspas@gmail.com>
2015-09-21 10:53:51 +0300

    Add logging to status queue clearing

--- skype-reporting/src/main/java/org/asg/report/CleanJob.java
+++ skype-reporting/src/main/java/org/asg/report/CleanJob.java
@@ -1,14 +1,18 @@
 package org.asg.report;
 
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public class CleanJob implements Job {
+	
+	private final static Logger LOGGER = Logger.getLogger(CleanJob.class);
 
 	public void execute(JobExecutionContext context)
 			throws JobExecutionException {
 		App.statuses.clear();		
+		LOGGER.info("Status Queue was just cleared");
 	}
 	
 
